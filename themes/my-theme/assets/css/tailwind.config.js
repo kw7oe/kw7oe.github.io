a1f76b27a70e3692cd9a6648647774ad03be4407
kw7oe <choongkwern@hotmail.com>
2022-04-27 10:29:02 +0800

    Add /fragments, styling and slight refactoring on hugo templates

--- themes/my-theme/assets/css/tailwind.config.js
+++ themes/my-theme/assets/css/tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
+        // 'sans': ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
         'serif': ['Lora', ...defaultTheme.fontFamily.serif],
       },
       typography: {
