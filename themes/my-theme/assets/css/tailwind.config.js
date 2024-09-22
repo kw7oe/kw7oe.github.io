1b02c8d54607255e92181134f7eb17cde68d5e35
kw7oe <choongkwern@hotmail.com>
2024-09-23 00:16:21 +0800

    update themes...

--- themes/my-theme/assets/css/tailwind.config.js
+++ themes/my-theme/assets/css/tailwind.config.js
@@ -8,42 +8,49 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        // 'sans': ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
-        'serif': ['Lora', ...defaultTheme.fontFamily.serif],
+	// 'sans': ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
+	'serif': ['Lora', ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+	df: {
+	  bg: "#ebe5df",
+	  "fg-400": "#4c4769",
+	  "fg-300": "#575279",
+	  "fg-200": "#625c87",
+	  "fg-100": "#a8a3b3",
+	}
       },
       typography: {
-        DEFAULT: {
-          css: {
-            // h1: {
-            //   marginTop: "5rem"
-            // },
-            h4: {
-              marginTop: "2.5rem",
-              marginBottom: "1.5rem"
-            },
-            "pre": {
-              "backgroundColor": "#edf2f7",
-              "marginTop": "0.75rem",
-              "marginBottom": "1.25rem",
-              "padding": "0.5rem",
-              "borderRadius": "0.25rem",
-              "lineHeight": "1.5",
-              "whiteSpace": "pre-wrap",
-              "overflowX": "auto",
-            },
-            "pre code": {
-              "padding": "0",
-              "color": "#333"
-            },
-            "code": {
-              "font-size": "0.875rem",
-              "background-color": "#edf2f7",
-              "padding": "0.25rem",
-              "border-radius": "0.25rem",
-              "width": "100%"
-            }
-          }
-        }
+	DEFAULT: {
+	  css: {
+	    h4: {
+	      marginTop: "2.5rem",
+	      marginBottom: "1.5rem"
+	    },
+	    "hr": {
+	      "borderColor": "#d8d0ca"
+	    },
+	    "pre": {
+	      "backgroundColor": "#e0dad5",
+	      "marginTop": "0.75rem",
+	      "marginBottom": "1.25rem",
+	      "borderRadius": "0.25rem",
+	      "lineHeight": "1.5",
+	      "whiteSpace": "pre-wrap",
+	      "overflowX": "auto",
+	    },
+	    "pre code": {
+	      "color": "#333"
+	    },
+	    "code": {
+	      "font-size": "0.875rem",
+	      "background-color": "#e0dad5",
+	      "padding": "0.25rem",
+	      "border-radius": "0.25rem",
+	      "width": "100%"
+	    }
+	  }
+	}
       }
     }
   },
